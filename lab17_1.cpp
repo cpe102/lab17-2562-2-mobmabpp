9985341d22624ecd852dc40696a1229cda5f5ec5
lab303-1 <lab303-1@labeng.local>
2020-03-06 17:26:56 +0700

    mobmablee

--- lab17_1.cpp
+++ lab17_1.cpp
@@ -1,1 +1,30 @@
-//Write your code here
+#include<iostream>
+#include<cstring>
+#include<fstream>
+using namespace std;
+
+int main(){
+    ifstream source;
+    source.open(C:\Users\lab303-1\lab17-2562-2-mobmabpp/name_score.txt);
+
+    string textline;
+
+    vector<string> list_name();
+    vector<string> list_score();
+    vector<string> list_grade();
+
+    while(getline(source,textline)){
+        cout<<"\n"; 
+        char format[]= %[^:]:%f,%f,%f;
+        char name[100]
+        float a,b,c;
+        sscanf(textline.c_str(),format,name,&a,&b,&c)
+        cout << name << "\n";
+        cout << a << " " << b << " " << c << "\n";
+
+        list_name.push_back()
+        list_score.push_back()
+        list_grade.push_back()
+}
+
+}
